752f1e26a2f9878cd606a5aa6d3e0986af05a38d
Alin Sanchez <edalin.sb.2908@gmail.com>
2020-10-06 19:02:17 -0500

    spinner added in fragment

--- app/src/main/java/com/ram/android_developer_test/ui/carpark/fragment/CarParkRegisterFragment.kt
+++ app/src/main/java/com/ram/android_developer_test/ui/carpark/fragment/CarParkRegisterFragment.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import com.ram.android_developer_test.R
 import com.ram.android_developer_test.databinding.FragmentRegisterBinding
 
-class CarParkRegisterFragment: Fragment() {
+class CarParkRegisterFragment: Fragment(), AdapterView.OnItemSelectedListener {
 
     lateinit var binding: FragmentRegisterBinding
 
@@ -30,6 +34,25 @@ class CarParkRegisterFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onSelectedRegister()
+    }
+
+    private fun onSelectedRegister() {
+        val spinner: Spinner = binding.spinnerRegister
+        ArrayAdapter.createFromResource(
+            requireContext(), R.array.register_option, R.layout.support_simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
     interface CarParkRegisterListener {
--- app/src/main/java/com/ram/android_developer_test/ui/carpark/fragment/CarParkRegistryFragment.kt
+++ app/src/main/java/com/ram/android_developer_test/ui/carpark/fragment/CarParkRegistryFragment.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import com.ram.android_developer_test.R
 import com.ram.android_developer_test.databinding.FragmentRegistryBinding
 
-class CarParkRegistryFragment: Fragment() {
+class CarParkRegistryFragment: Fragment(), AdapterView.OnItemSelectedListener {
 
     lateinit var binding: FragmentRegistryBinding
 
@@ -30,6 +34,25 @@ class CarParkRegistryFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onSelectedRegistry()
+    }
+
+    private fun onSelectedRegistry() {
+        val spinner: Spinner = binding.spinnerRegistry
+        ArrayAdapter.createFromResource(
+            requireContext(), R.array.registry_option, R.layout.support_simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
     interface CarParkRegistryListener {
